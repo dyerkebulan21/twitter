7287f89d9378b66fc8bc661efefcb32fcf8a494a
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-04-13 21:11:43 +0600

    Modal add

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,14 +3,53 @@ import Typography from '@material-ui/core/Typography'
 import {makeStyles} from '@material-ui/core'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import Button from '@material-ui/core/Button'
+import SearchIcon from '@material-ui/icons/Search';
+import PeopleIcon from '@material-ui/icons/People';
+import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
 const useStyles = makeStyles((theme) => ({
     wrapper: {
         display: 'flex',
         height: 'calc(100vh - 84px)',
     },
     blueSide: {
-        backgroundColor: '#1DA1F2',
+        backgroundColor: '#71C9F8',
         flex: '0 0 50%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
+        overflow: 'hidden'
+    },
+    blueSideBigIcon: {
+        position: 'absolute',
+        top: '50%',
+        left: '53%',
+        width: '350%',
+        height: '350%',
+        transform: 'translate(-50%, -50%)'
+    },
+    blueSideInfo: {
+        listStyle: 'none',
+        position: 'relative',
+        width: 380,
+        padding: 0,
+        margin: 0,
+        '& h6': {
+            color: 'white',
+            fontWeight: 700,
+            fontSize: 20,
+            display: 'flex',
+            aligItems: 'center',
+            
+        }
+    },
+
+    blueSideListInfoItem:{
+        marginBottom: 40,
+    },
+    blueSideInfoIcon: {
+        marginRight: 30,
+        fontSize: 32
     },
     loginSide: {
         flex: '0 0 50%',
@@ -39,19 +78,16 @@ function SignIn() {
     return (
         <div className={classes.wrapper}>
             <div className={classes.blueSide}>
-                <ul> 
-                    <li>
-                        <Typography>Читайте о том что вам интересно</Typography>
+            <TwitterIcon className={classes.blueSideBigIcon} color="primary"/>
+                <ul className={classes.blueSideInfo}> 
+                    <li className={classes.blueSideListInfoItem}>
+                        <Typography variant="h6"><SearchIcon className={classes.blueSideInfoIcon}/>Читайте о том что вам интересно</Typography>
                     </li> 
-                </ul>
-                <ul>
-                    <li>
-                    <Typography>Узнайте о чем говорят в мире</Typography>
+                    <li className={classes.blueSideListInfoItem}>
+                    <Typography variant="h6"><PeopleIcon className={classes.blueSideInfoIcon}/>Узнайте о чем говорят в мире</Typography>
                         </li>
-                </ul>
-                <ul>
-                    <li>
-                    <Typography> Присоединяйтесь к общению </Typography>
+                    <li className={classes.blueSideListInfoItem}>
+                    <Typography variant="h6"><ModeCommentOutlinedIcon className={classes.blueSideInfoIcon}/> Присоединяйтесь к общению </Typography>
                         </li>
                 </ul>
             </div>
