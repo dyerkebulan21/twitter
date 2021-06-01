0fba2170e0ad33afcc381d602d83116f2d05c4b6
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-06-01 19:01:09 +0600

    circular progress tweet

--- src/pages/Home/components/FullTweet/index.tsx
+++ src/pages/Home/components/FullTweet/index.tsx
@@ -1,9 +1,16 @@
+import { CircularProgress } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Tweet } from "../../../../components/Tweet";
-import { fetchTweetData } from "../../../../store/ducks/tweet/actionCreators";
-import { selectedTweetData } from "../../../../store/ducks/tweet/selectors";
+import {
+  fetchTweetData,
+  setTweet,
+} from "../../../../store/ducks/tweet/actionCreators";
+import {
+  selectedTweetData,
+  selectIsTweetLoading,
+} from "../../../../store/ducks/tweet/selectors";
 import { useHomeStyles } from "../../theme";
 
 export const FullTweet: React.FC = (): React.ReactElement | null => {
@@ -12,15 +19,27 @@ export const FullTweet: React.FC = (): React.ReactElement | null => {
   const params: { id: string } = useParams();
   const id = params.id;
   const tweetData = useSelector(selectedTweetData);
+  const isTweetLoading = useSelector(selectIsTweetLoading);
   React.useEffect(() => {
     if (id) {
       dispatch(fetchTweetData(id));
     }
+    return () => {
+      dispatch(setTweet(undefined));
+    };
   }, [dispatch]);
-  if (!tweetData) {
-    return null;
+
+  if (isTweetLoading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: 30 }}>
+        <CircularProgress color="primary" />
+      </div>
+    );
   }
-  console.log(tweetData)
-  //@ts-ignore
-  return <Tweet classes={classes} {...tweetData} />;
+  if (tweetData) {
+    //@ts-ignore
+    return <Tweet classes={classes} {...tweetData} />;
+  }
+
+  return null;
 };
