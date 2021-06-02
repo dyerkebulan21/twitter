9576011fea34e235555baa38e8be60c6879c4f05
dyerkebulan21 <dyerkebulan21@gmail.com>
2021-06-02 21:56:14 +0600

    add tweet redux

--- src/store/ducks/tweets/actionCreators.ts
+++ src/store/ducks/tweets/actionCreators.ts
@@ -1,16 +1,22 @@
 import {Action} from 'redux'
-import { LoadingState, TweetsState } from "./contracts/state";
+
+import { LoadingState, Tweet, TweetsState } from "./contracts/state";
 
 export enum TweetsActionType {
     SET_TWEETS = 'tweets/SET_TWEETS',
     FETCH_TWEETS = 'tweets/FETCH_TWEETS',
     LOADING_TWEETS = 'tweets/LOADING_TWEETS',
-    
+    ADD_TWEETS = 'tweets/ADD_TWEET'
 }
 
 export interface SetTweetsActionInterface extends Action<TweetsActionType> {
     type: TweetsActionType.SET_TWEETS,
     payload: TweetsState['items']
+}
+
+export interface AddTweetsActionInterface extends Action<TweetsActionType> {
+    type: TweetsActionType.ADD_TWEETS,
+    payload: Tweet
 }
 
 export interface SetTweetsLoadingActionInterface extends Action<TweetsActionType> {
@@ -34,6 +40,11 @@ export interface RootState {
 
 export const setTweets = (payload: TweetsState['items']): SetTweetsActionInterface => ({
     type: TweetsActionType.SET_TWEETS,
+    payload
+})
+
+export const addTweet = (payload: Tweet):AddTweetsActionInterface => ({
+    type: TweetsActionType.ADD_TWEETS,
     payload
 })
 
