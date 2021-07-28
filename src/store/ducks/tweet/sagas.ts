7dbd2b1988262231e0f71a73f5aea382f24af40d
Yerkebulan <dyerkebulan21@gmail.com>
2021-07-28 19:14:46 +0600

    features/format date adding

--- src/store/ducks/tweet/sagas.ts
+++ src/store/ducks/tweet/sagas.ts
@@ -1,19 +1,25 @@
-import {call, put,  takeLatest} from 'redux-saga/effects'
-import { TweetsApi } from '../../../services/api/tweetsApi'
-import { FetchTweetDataActionInterface, setLoadingTweet, setTweet, TweetActionType, } from './actionCreators'
-import { LoadingState } from './contracts/state'
+import { call, put, takeLatest } from "redux-saga/effects";
+import { TweetsApi } from "../../../services/api/tweetsApi";
+import { Tweet } from "../tweets/contracts/state";
+import {
+  FetchTweetDataActionInterface,
+  setLoadingTweet,
+  setTweet,
+  TweetActionType,
+} from "./actionCreators";
+import { LoadingState } from "./contracts/state";
 
-export function* fetchTweetRequest({payload: tweetId}: FetchTweetDataActionInterface){
-  
-    try{
-        //@ts-ignore
-        const item: Tweet[] = yield call(TweetsApi.fetchTweet, tweetId)
-        yield put(setTweet(item[0]))
-    }catch(err) {
-        yield put(setLoadingTweet(LoadingState.ERROR))
-    }
+export function* fetchTweetRequest({
+  payload: tweetId,
+}: FetchTweetDataActionInterface) {
+  try {
+    const item: Tweet = yield call(TweetsApi.fetchTweet, tweetId);
+    yield put(setTweet(item));
+  } catch (err) {
+    yield put(setLoadingTweet(LoadingState.ERROR));
+  }
 }
 
 export function* tweetSaga() {
-    yield takeLatest(TweetActionType.FETCH_DATA, fetchTweetRequest)
+  yield takeLatest(TweetActionType.FETCH_DATA, fetchTweetRequest);
 }
