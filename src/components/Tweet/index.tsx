4250e0aadb9bbe5a980be64ec16a16497c5ab87f
Yerkebulan <dyerkebulan21@gmail.com>
2021-08-03 17:02:06 +0600

    features:edit tweet

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -11,7 +11,7 @@ import classNames from "classnames";
 import React from "react";
 import { useHomeStyles } from "../../pages/Home/theme";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import Comment from "@material-ui/icons/ChatBubble";
 import RepeatIcon from "@material-ui/icons/Repeat";
@@ -41,8 +41,15 @@ export const Tweet: React.FC<TweetProps> = ({
 }): React.ReactElement => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
+  const history = useHistory();
+
+  const handleClickTweet = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    history.push(`/home/tweet/${_id}`);
+  };
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
     setAnchorEl(event.currentTarget);
   };
 
@@ -50,14 +57,18 @@ export const Tweet: React.FC<TweetProps> = ({
     setAnchorEl(null);
   };
   return (
-    <Link to={`/home/tweet/${_id}`} className={classes.tweetWrapper}>
+    <a
+      onClick={handleClickTweet}
+      href={`/home/tweet/${_id}`}
+      className={classes.tweetWrapper}
+    >
       <Paper className={classNames(classes.tweetsHeader, classes.tweet)}>
         <Grid container spacing={3}>
           <Grid item xs={1}>
             <Avatar alt="Remy Sharp" src={user.avatarUrl} />
           </Grid>
           <Grid item xs={11}>
-            <Typography>
+            <Typography className={classes.tweetHeader}>
               <div>
                 <b>{user.fullname}</b>{" "}
                 <span className={classes.tweetUserName}>@{user.username}</span>
@@ -122,6 +133,6 @@ export const Tweet: React.FC<TweetProps> = ({
           </Grid>
         </Grid>
       </Paper>
-    </Link>
+    </a>
   );
 };
