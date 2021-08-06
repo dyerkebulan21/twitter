e36a652dca5ccc43a864c0928e569d45c5f04ade
Yerkebulan <dyerkebulan21@gmail.com>
2021-08-06 23:18:15 +0600

    features:signin components

---
+++ src/pages/SignIn/components/LoginModal/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const LoginModal: React.FC = () => {
+  return <div></div>;
+};
+
+export { LoginModal };
---
+++ src/pages/SignIn/components/RegisterModal/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const RegisterModal: React.FC = () => {
+  return <div></div>;
+};
+
+export { RegisterModal };
